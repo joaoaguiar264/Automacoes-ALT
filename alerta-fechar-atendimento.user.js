6fa25bb9e7666c6080746d2dc3e6003d883bba30
Joao Vinicius <joaoviniciusaguiar@gmail.com>
2026-02-17 17:31:04 -0300

    Update alerta-fechar-atendimento.user.js

--- alerta-fechar-atendimento.user.js
+++ alerta-fechar-atendimento.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         alerta-fechar-atendimento aaaa
+// @name         alerta-fechar-atendimento
 // @namespace    http://tampermonkey.net/
 // @version      1.3
 // @description  Alerta ao fechar atendimento com motivo "Dúvidas/Informações" no INT6.
