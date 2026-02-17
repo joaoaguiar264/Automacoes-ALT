a3a7302744a6868c23000d5afa39d243bcda739e
Joao Vinicius <joaoviniciusaguiar@gmail.com>
2026-02-17 17:22:09 -0300

    Update alerta-fechar-atendimento.js

--- alerta-fechar-atendimento.js
+++ alerta-fechar-atendimento.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         alerta-fechar-atendimento aaaa
+// @name         alerta-fechar-atendimento
 // @namespace    http://tampermonkey.net/
 // @version      1.3
 // @description  Alerta ao fechar atendimento com motivo "Dúvidas/Informações" no INT6.
-// @author       none
+// @author       eu
 // @match        *://integrator6.gegnet.com.br/*
 // @match        *//integrator6.alt.com.br/*
 // @updateURL    https://raw.githubusercontent.com/joaoaguiar264/Automacoes-ALT/refs/heads/main/alerta-fechar-atendimento.js
