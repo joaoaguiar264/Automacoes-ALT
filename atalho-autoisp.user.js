9ad19a24c92f1ed32328e273a8e1c147c95d2638
Joao Vinicius <joaoviniciusaguiar@gmail.com>
2026-02-19 16:34:08 -0300

    Update atalho-autoisp.user.js

--- atalho-autoisp.user.js
+++ atalho-autoisp.user.js
@@ -6,8 +6,8 @@
 // @author       Matheus Eger
 // @match        https://integrator6.gegnet.com.br/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gegnet.com.br
-// @downloadURL  https://github.com/wrGGsiq/scriptsGG/raw/main/Atalho%20para%20o%20AutoISP.user.js
-// @updateURL    https://github.com/wrGGsiq/scriptsGG/raw/main/updates/Atalho%20para%20o%20AutoISP.meta.js
+// @downloadURL  https://github.com/joaoaguiar264/Automacoes-ALT/raw/refs/heads/main/atalho-autoisp.user.js
+// @updateURL    https://github.com/joaoaguiar264/Automacoes-ALT/raw/refs/heads/main/atalho-autoisp.user.js
 // @grant        GM_setClipboard
 // ==/UserScript==
 
