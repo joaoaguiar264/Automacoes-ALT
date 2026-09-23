eed23a0280672a2dc1ff9dcf087e3d81353d98c6
Joao Vinicius <joaoviniciusaguiar@gmail.com>
2026-09-23 16:54:53 -0300

    Bump version to 0.7.1

--- Zendesk - ALT Copilot.user.js
+++ Zendesk - ALT Copilot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk - ALT Copilot
 // @namespace    alt.copilot
-// @version      0.7.0
+// @version      0.7.1
 // @description  Sistema de presets/mensagens prontas para atendimento ALT
 // @author       João Aguiar
 // @match        https://brasiltecparsupport.zendesk.com/agent/*
