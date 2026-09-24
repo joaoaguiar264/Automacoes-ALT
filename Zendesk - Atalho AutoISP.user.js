71fda1bc3c9e77382bd127cc8c974c4fffd03dca
Joao Vinicius <joaoviniciusaguiar@gmail.com>
2026-09-23 23:23:25 -0300

    Bump version from 1.0 to 1.1

--- Zendesk - Atalho AutoISP.user.js
+++ Zendesk - Atalho AutoISP.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk - Atalho AutoISP
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Abre automaticamente no AutoISP o cliente do ticket ativo do Zendesk
 // @author       ALT
 // @match        https://brasiltecparsupport.zendesk.com/agent/*
