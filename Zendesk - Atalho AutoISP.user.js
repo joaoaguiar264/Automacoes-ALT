cb40ea9bda27f958bd6c87be23335fbcba0b39d1
Joao Vinicius <joaoviniciusaguiar@gmail.com>
2026-09-23 23:23:12 -0300

    Add update and download URLs to user script

--- Zendesk - Atalho AutoISP.user.js
+++ Zendesk - Atalho AutoISP.user.js
@@ -6,8 +6,8 @@
 // @author       ALT
 // @match        https://brasiltecparsupport.zendesk.com/agent/*
 // @match        https://autoisp.gegnet.com.br/subscribers*
-// @updateURL
-// @downloadURL
+// @updateURL    https://github.com/joaoaguiar264/Automacoes-ALT/raw/refs/heads/main/Zendesk%20-%20Atalho%20AutoISP.user.js
+// @downloadURL    https://github.com/joaoaguiar264/Automacoes-ALT/raw/refs/heads/main/Zendesk%20-%20Atalho%20AutoISP.user.js
 // @icon         https://github.com/joaoaguiar264/Automacoes-ALT/raw/refs/heads/main/icon.png
 // @grant        none
 // @run-at       document-idle
